4b087884da8ae2ad1ff64dffc770584bf1a17771
Rocky Randalls <rrandalls@gmail.com>
2015-01-21 09:23:48 -0500

    Add numbers to tests

--- currency_test.rb
+++ currency_test.rb
@@ -3,33 +3,33 @@ require 'minitest/pride'
 require './currency'
 
 class CurrencyTest < Minitest::Test
-    def test_if_currency_class_exists
+  def test_00_if_currency_class_exists
       assert Currency
-    end
+  end
 
-    def test_currency_has_creation_arguments
+  def test_01_currency_has_creation_arguments
       assert Currency.new("", "")
-    end
+  end
 
-  def test_different_currecy_object_equality
+  def test_02_different_currecy_object_equality
     us_dollars = Currency.new(100, "USD")
     ca_dollars = Currency.new(100, "USD")
     assert_equal true, us_dollars == ca_dollars
   end
 
-  def test_if_like_currencies_can_be_added
+  def test_03_if_like_currencies_can_be_added
     us_dollars = Currency.new(100, "USD")
     ca_dollars = Currency.new(50, "USD")
     assert_equal 150, us_dollars + ca_dollars
   end
 
-  def test_if_like_currencies_can_be_subtracted
+  def test_04_if_like_currencies_can_be_subtracted
     us_dollars = Currency.new(100, "USD")
     ca_dollars = Currency.new(50, "USD")
     assert_equal 50, us_dollars - ca_dollars
   end
 
-  def test_error_raised_for_mismatched_codes
+  def test_05_error_raised_for_mismatched_codes
     us_dollars = Currency.new(100, "USD")
     abu_dhabi = Currency.new(75, "AED")
 
@@ -42,7 +42,7 @@ class CurrencyTest < Minitest::Test
     end
   end
 
-  def test_multiplying_currency_by_fixnum_or_float
+  def test_06_multiplying_currency_by_fixnum_or_float
     us_dollars = Currency.new(100, "USD")
     assert_equal 1000, us_dollars * 10
   end
