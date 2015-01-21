cfed8a12ed1e1f651bd067855d236a73925efc72
Rocky Randalls <rrandalls@gmail.com>
2015-01-20 19:14:23 -0500

    Modifed currency class and test, all required methods/tests working..I HAS ALL THE MONIES

--- currency.rb
+++ currency.rb
@@ -1,5 +1,43 @@
-class Currency
-  def initialize(amount, currency_code)
+class DifferentCurrencyCodeError < StandardError
 
+end
+
+class Currency
+  attr_reader :amount, :code
+  def initialize(amount, code)
+    @amount = amount
+    @code = code
   end
+
+  def ==(currency_object)
+    if currency_object.amount == @amount &&
+      currency_object.code == @code
+      return true
+    else
+      return false
+    end
+  end
+
+  def +(currency_object)
+    if currency_object.code == @code
+      currency_object.amount + @amount
+    else
+      raise DifferentCurrencyCodeError, "The currency codes between these do not match."
+      return nil
+    end
+  end
+
+  def -(currency_object)
+    if currency_object.code == @code
+      @amount - currency_object.amount
+    else
+      raise DifferentCurrencyCodeError, "The currency codes between these do not match."
+      return nil
+    end
+  end
+
+  def *(number)
+    @amount * number
+  end
+
 end
--- currency_test.rb
+++ currency_test.rb
@@ -3,13 +3,47 @@ require 'minitest/pride'
 require './currency'
 
 class CurrencyTest < Minitest::Test
-  def test_if_currency_class_exists
-    assert Currency
+    def test_if_currency_class_exists
+      assert Currency
+    end
+
+    def test_currency_has_creation_arguments
+      assert Currency.new("", "")
+    end
+
+  def test_different_currecy_object_equality
+    us_dollars = Currency.new(100, "USD")
+    ca_dollars = Currency.new(100, "USD")
+    assert_equal true, us_dollars == ca_dollars
   end
 
-  def test_currency_has_creation_arguments
-    assert Currency.new(100, "usd")
+  def test_if_like_currencies_can_be_added
+    us_dollars = Currency.new(100, "USD")
+    ca_dollars = Currency.new(50, "USD")
+    assert_equal 150, us_dollars + ca_dollars
   end
 
+  def test_if_like_currencies_can_be_subtracted
+    us_dollars = Currency.new(100, "USD")
+    ca_dollars = Currency.new(50, "USD")
+    assert_equal 50, us_dollars - ca_dollars
+  end
 
+  def test_error_raised_for_mismatched_codes
+    us_dollars = Currency.new(100, "USD")
+    abu_dhabi = Currency.new(75, "AED")
+
+    assert_raises(DifferentCurrencyCodeError) do
+      us_dollars + abu_dhabi
+    end
+
+    assert_raises(DifferentCurrencyCodeError) do
+      us_dollars - abu_dhabi
+    end
+  end
+
+  def test_multiplying_currency_by_fixnum_or_float
+    us_dollars = Currency.new(100, "USD")
+    assert_equal 1000, us_dollars * 10
+  end
 end
